cc81df13eb048b3b7b0bc705a861504c765a7c34
AaronZamora <aaron1319104668@uptecamac.edu.mx>
2021-09-27 14:35:42 -0500

    Tienda proyecto

---
+++ Arreglos/main.js
@@ -0,0 +1,47 @@
+var elemento=[];
+var colores=["red","green","blue","yellow"];
+
+var ME={
+    "informacion":{
+        "Nombre":"Aaron Gamaliel",
+        "Edad":26,
+        "Carrera":"Ing. en Software"
+    },
+    "Musica":{
+        "Generos":{
+            "Dubstep":{
+                "Canciones":{
+                    "1, ":"No regrets",
+                    "2. ":"Mega Punch",
+                    "3. ":"Mercha",
+                    "Artistas":{
+                        "1. ":"Ivori",
+                        "2. ":"Calcium",
+                        "3. ":"Infekt"
+                    }
+                },
+                "Albumnes":{
+                    "1. ":"Black Friday Vol.11",
+                    "2. ":"Mega Punch EP",
+                    "3. ":"Kaizen EP"
+                }
+            },
+            "Hardstyle":{
+                "Canciones":{
+                    "1, ":"Madrid",
+                    "2. ":"Mighty Methods",
+                    "3. ":"Soild Stigma",
+                    "Artistas":{
+                        "1. ":"Angerfist",
+                        "2. ":"Angerfist",
+                        "3. ":"Angerfist"
+                    }
+                },
+                "Albunes":{
+
+                }
+            }            
+        }
+    }
+}
+
